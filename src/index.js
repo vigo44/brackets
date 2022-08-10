61da05a3e19275ca930667d2cbb0abcc58d2ca2a
vigo44 <vvigo@rambler.ru>
2022-08-11 02:33:37 +0400

    add: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrStr = Array.from(str);
+  let stack=[];
+  let bracketOpen;
+  let result= true;
+  arrStr.forEach(item=>{
+    for (let i=0; i<bracketsConfig.length; i++){
+      if (item==bracketsConfig[i][1]){
+        bracketOpen = stack.pop();
+          if (bracketOpen !== bracketsConfig[i][0]) {
+            if (item == bracketsConfig[i][0]) {
+              if  (bracketOpen) {stack.push(bracketOpen)};
+              
+            }else {
+              result=false;
+            }
+                       
+          } else {
+            return;
+          }
+  
+        } 
+       
+    }
+
+    for (let i=0; i<bracketsConfig.length; i++){
+        if (item==bracketsConfig[i][0]){
+          stack.push(item);
+          
+        } 
+    } 
+     
+  })
+  if (stack.length !==0){
+      return false;
+    }
+    return result;
+
 }
